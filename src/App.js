3e1ff8781b8aecd5133d26aaa7e2d786d4a6e544
Francine <franmelopepe2@gmail.com>
2022-06-15 15:08:08 +0200

    fetch data with axios (JS)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        setLoading(true);
+
+        const response = await axios.get(
+          "https:jsonplaceholder.typicode.com/posts"
+        );
+
+        setData(response.data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    setLoading(true);
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading && "Loading---"}
+
+      {error && "Oops, could not fetch posts, please try again later"}
+
+      {data &&
+        data.map((post) => {
+          const { id, title, body } = post;
+
+          return (
+            <article key={id}>
+              <p>{title}</p>
+              <p>{id}</p>
+              <p>{body}</p>
+            </article>
+          );
+        })}
+    </>
   );
 }
 
